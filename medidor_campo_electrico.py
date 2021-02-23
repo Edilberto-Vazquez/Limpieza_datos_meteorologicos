d7d74602dfbd67bb4aaa82ff1180bce7722d7c1e
Edilberto <edi_vazquez12@hotmail.com>
2021-02-23 07:32:27 -0600

    se cambio ThreadPool por ProcessPool y se usan fstring en lugar de concatenar

--- medidor_campo_electrico.py
+++ medidor_campo_electrico.py
@@ -1,37 +1,38 @@
 import pandas as pd
 import glob
 import os
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 import threading
 import time
 
 
-directorio = glob.glob('C:/Users/hxh_1/Desktop/elementos_prueba/*.efm')
+directorio = glob.glob('D:/Tesis/Medidor_parque/*.efm')
 
 #print(directorio)
 
 def concat_fecha(directorio):
     directorio = [directorio]
-    print(directorio)
     for archivo in directorio:
         fecha = os.path.basename(archivo).lstrip("INAOE parque-").rstrip(".efm")
         fecha = fecha[4:]+"-"+fecha[0:2]+"-"+fecha[2:4]
+        fecha = f'{fecha[4:]}-{fecha[0:2]}-{fecha[2:4]}'
         with open(archivo, 'r', encoding='UTF-8') as f_read:
             n_lineas = f_read.readlines()
-            with open('C:/Users/hxh_1/Desktop/elementos_prueba/conjunto_campo_electrico.csv', 'a', encoding='UTF-8') as f_write:
-                f_write.write('Fecha, Polaridad, NivelCE, FalloRotor')
-                for linea in range(len(n_lineas)):
-                    linea_n = n_lineas[linea].replace(',0',',correcto').replace(',1',',fallo').replace('+', 'positivo,').replace('-', 'negativo,')
-                    f_write.write(fecha+' '+linea_n)
+            with open(f'E:/Datos_tratados/Conjuntos_datos_meteorologicos/{fecha}.csv', 'a', encoding='UTF-8') as f_write:
+                f_write.write('Fecha, Polaridad, NivelCE, FalloRotor\n')
+                for linea in n_lineas:
+                    linea_n = linea.replace(',0',',correcto').replace(',1',',fallo').replace('+', 'positivo,').replace('-', 'negativo,')
+                    f_write.write(f'{fecha} {linea_n}')
 
 if __name__ == "__main__":
     
     t1 = time.time()
     
-    with ThreadPoolExecutor() as executor:
-        for i, result in enumerate(executor.map(concat_fecha, directorio)):
-            print(f'procesado {directorio[i]}')
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        executor.map(concat_fecha, directorio)
+        '''for i, result in enumerate(executor.map(concat_fecha, directorio)):
+            print(f'procesado {directorio[i]}')'''
     
     t2 = time.time()
-    elapsed = round(t2 - t1, 4)
-    print(f"Processed data in {elapsed} seconds.")
+    t_transcurrido = round(t2 - t1, 4)
+    print(f'Archivos procesados en {t_transcurrido} segundos.')
