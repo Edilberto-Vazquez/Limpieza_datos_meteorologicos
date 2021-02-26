afa07105b9e73832a809672fae2765ad7fe8a6ee
Edilberto <edi_vazquez12@hotmail.com>
2021-02-26 00:02:03 -0600

    negativo

--- redondeo_fecha_medidor_campo_electrico.py
+++ redondeo_fecha_medidor_campo_electrico.py
@@ -8,17 +8,17 @@ moda_array = []
 
 for n_archivo in directorio:
     archivo = pd.read_csv(n_archivo, sep=',', decimal='.' ,encoding='UTF-8')
-    negativo = archivo[archivo['Polaridad'] == 'negativo']
+    negativo = archivo[archivo.Polaridad == 'negativo']
     fecha = negativo.iloc[0, 0]
     for n_linea in range(len(negativo)):
         if negativo.iloc[n_linea, 0] == fecha:
             if n_linea == len(negativo)-1:
-                new_archivo = new_archivo.append({'Fecha':fecha, 'Polaridad':negativo.iloc[n_linea, 1], 'NivelCE':st.mean(moda_array), 'FalloRotor':negativo.iloc[n_linea, 3]}, ignore_index=True)
+                new_archivo = new_archivo.append({'Fecha':fecha, 'Polaridad':negativo.iloc[n_linea, 1], 'NivelCE':round(st.mean(moda_array),2), 'FalloRotor':negativo.iloc[n_linea, 3]}, ignore_index=True)
             else:
                 moda_array.append(negativo.iloc[n_linea, 2])
         else:
             print(st.mode(moda_array))
-            new_archivo = new_archivo.append({'Fecha':fecha, 'Polaridad':negativo.iloc[n_linea-1, 1], 'NivelCE':st.mean(moda_array), 'FalloRotor':negativo.iloc[n_linea-1, 3]}, ignore_index=True)
+            new_archivo = new_archivo.append({'Fecha':fecha, 'Polaridad':negativo.iloc[n_linea-1, 1], 'NivelCE':round(st.mean(moda_array),2), 'FalloRotor':negativo.iloc[n_linea-1, 3]}, ignore_index=True)
             moda_array = [negativo.iloc[n_linea, 2]]
             fecha = negativo.iloc[n_linea, 0]
 new_archivo.to_csv('F:/Archivos/DataSets/Tesis/Datos_Procesados/conjunto_campo_electrico.csv', sep=',', decimal='.', index=None, encoding='UTF-8')
